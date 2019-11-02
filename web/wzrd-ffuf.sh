62386736edd9d8ae6d753d7909ef0246f9857877
fuzzerk <prezzure@protonmail.com>
2019-11-02 22:28:27 +0000

    update gobuster and add ffuf and wfuzz

---
+++ web/wzrd-ffuf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# ffuf -c -u $targ -w $list -k -o ffuf-$input-$(echo $targ | sed 's/\//_/g' | sed 's/\:/_/g').txt -t 10 -H "$UA" -ac
+
+UA="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36 (bugcrowd&h1)"
+
+interactive=
+filename=~/sysinfo_page.html
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -f | --file )           shift
+                                filename=$1
+                                ;;
+        -i | --interactive )    interactive=1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+echo "file: $filename"
+
+exit 0
