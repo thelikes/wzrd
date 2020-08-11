7a3831cef482b22a76c0686e8dc5f5ea38caaa8f
thelikes <root@local>
2020-08-10 22:20:19 -0400

    update template

--- templates/wzrd-x.sh
+++ templates/wzrd-x.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-interactive=
-filename=~/sysinfo_page.html
+usage () {
+    echo "Usage: "
+}
 
 while [ "$1" != "" ]; do
     case $1 in
         -f | --file )           shift
                                 filename=$1
+                                ;;
+        -o | --output )         shift
+                                output=$1
                                 ;;
         -i | --interactive )    interactive=1
                                 ;;
@@ -19,6 +23,11 @@ while [ "$1" != "" ]; do
     shift
 done
 
-echo "file: $filename"
+if [[ ! -f $filename ]] ; then 
+    echo "[!] Error: "
+    exit 1
+fi
+
+# do stuff
 
 exit 0
