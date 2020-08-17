1bff1f71b5d94d6070ce81888fcb58025d66a7f4
thelikes <root@local>
2020-08-16 19:27:18 -0700

    update wzrd-httpx.sh

--- web/wzrd-httpx.sh
+++ web/wzrd-httpx.sh
@@ -2,12 +2,16 @@
 
 usage () {
     echo "Usage: ./wzrd-httpx.sh -t <target>"
+    echo "       -o <output> (optional)"
 }
 
 while [ "$1" != "" ]; do
     case $1 in
         -t | --target )         shift
                                 targ=$1
+                                ;;
+        -o | --output )         shift
+                                outfile=$1
                                 ;;
         -h | --help )           usage
                                 exit
@@ -24,12 +28,14 @@ if [[ "$targ" == "" ]] ; then
     exit 1
 fi
 
-out=httpx-$(echo $targ | sed 's/:\/\//./g'|sed 's/\/$//g').txt
+if [[ "$outfile" != "" ]] ; then
+    out_str="-o $outfile"
+fi
 
 if [[ -f $targ ]]; then
-    httpx -l $targ -H "User-Agent: $UA" -no-color -o $out -threads 20 -ports 80,443,8080,8443 -title -content-length -status-code -web-server -silent
+    httpx -l $targ -H "User-Agent: $UA" -no-color $out_str -threads 50 -ports 80,443,8080,8443 -title -content-length -status-code -web-server -silent
 else
-    echo $targ | httpx -H "User-Agent: $UA" -no-color -o $out -threads 20 -ports 80,443,8080,8443 -title -content-length -status-code -web-server -silent
+    echo $targ | httpx -H "User-Agent: $UA" -no-color $out_str -threads 50 -ports 80,443,8080,8443 -title -content-length -status-code -web-server -silent
 fi
 
 exit 0
